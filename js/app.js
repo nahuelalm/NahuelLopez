509b6d5c6cdef1416eedc923aa580dd05ae69c2a
Nahuel López <nahuioh.1891@gmail.com>
2024-03-25 10:28:07 -0300

    WIP COLORES Y BACKGROUND ASPECTO NEGRO BLANCO

--- js/app.js
+++ js/app.js
@@ -22,3 +22,32 @@
   elementsToAnimate.forEach(element => {
     observer.observe(element);
   });
+
+
+document.getElementById('themeToggle').addEventListener('change', function() {
+  if (this.checked) {
+    document.body.classList.add('theme-dark');
+    document.body.classList.remove('theme-light');
+  } else {
+    document.body.classList.add('theme-light');
+    document.body.classList.remove('theme-dark');
+  }
+});  
+document.addEventListener('DOMContentLoaded', function() {
+  const themeToggle = document.getElementById('themeToggle');
+  const sunIcon = document.querySelector('.fa-sun');
+  const moonIcon = document.querySelector('.fa-moon');
+  
+  // Agregar un event listener para detectar cambios en el interruptor
+  themeToggle.addEventListener('change', function() {
+      if (themeToggle.checked) {
+          // Modo oscuro activado
+          sunIcon.classList.add('hidden');
+          moonIcon.classList.remove('hidden');
+      } else {
+          // Modo claro activado
+          sunIcon.classList.remove('hidden');
+          moonIcon.classList.add('hidden');
+      }
+  });
+});
