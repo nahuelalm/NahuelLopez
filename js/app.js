e3179f68a3f957f67a7acccb4bef943e7cb9d80d
Nahuel López <nahuioh.1891@gmail.com>
2024-03-27 20:13:02 -0300

    fix

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ elementsToAnimate.forEach(element => {
 });
 
 document.getElementById('themeToggle').addEventListener('change', function () {
-  if (this.checked) {
+  if (!this.checked) {
     document.body.classList.add('theme-dark');
     document.body.classList.remove('theme-light');
   } else {
@@ -42,39 +42,13 @@ document.addEventListener('DOMContentLoaded', function () {
   const sunIcon = document.querySelector('#sun-icon');
   const moonIcon = document.querySelector('#moon-icon');
 
+  // Invertir los estados iniciales de los iconos
+  sunIcon.classList.add('hidden');
+  moonIcon.classList.remove('hidden');
 
   // Agregar un event listener para detectar cambios en el interruptor
   themeToggle.addEventListener('change', function () {
-    if (themeToggle.checked) {
-      // Modo oscuro activado
-      sunIcon.classList.add('hidden');
-      moonIcon.classList.remove('hidden');
-    } else {
-      // Modo claro activado
-      sunIcon.classList.remove('hidden');
-      moonIcon.classList.add('hidden');
-    }
-  });
-});
-document.getElementById('themeToggleDesktop').addEventListener('change', function () {
-  if (this.checked) {
-    document.body.classList.add('theme-dark');
-    document.body.classList.remove('theme-light');
-  } else {
-    document.body.classList.add('theme-light');
-    document.body.classList.remove('theme-dark');
-  }
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-  const themeToggleDesktop = document.getElementById('themeToggleDesktop');
-  const sunIcon = document.querySelector('#sun-icon-desktop');
-  const moonIcon = document.querySelector('#moon-icon-desktop');
-
-
-  // Agregar un event listener para detectar cambios en el interruptor
-  themeToggleDesktop.addEventListener('change', function () {
-    if (themeToggleDesktop.checked) {
+    if (!themeToggle.checked) {
       // Modo oscuro activado
       sunIcon.classList.add('hidden');
       moonIcon.classList.remove('hidden');
@@ -86,18 +60,4 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-function handleResize() {
-  if (window.innerWidth >= 860) {
-    // Si la ventana es menor que 786px, ocultar navbar y mostrar div
-    document.getElementById('navbar').style.display = 'none';
-    document.getElementById('navbar-desktop').style.display = 'flex';
-  } else {
-    // Si la ventana es mayor o igual a 786px, mostrar navbar y ocultar div
-    document.getElementById('navbar').style.display = 'flex';
-    document.getElementById('navbar-desktop').style.display = 'none';
-  }
-}
 
-
-// Llama a handleResize() al cargar la página y en cada cambio de tamaño de la ventana
-window.onload = window.onresize = handleResize;
