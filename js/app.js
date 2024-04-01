d01e79dd501d83156c9184121c87cded173a97cd
Nahuel López <nahuioh.1891@gmail.com>
2024-04-01 20:31:18 -0300

    mejoras pagina WIP

--- js/app.js
+++ js/app.js
@@ -1,32 +1,3 @@
-// Función para animar los elementos cuando entran en el viewport
-function animateOnScroll(entries, observer) {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('animated-on-scroll');
-      observer.unobserve(entry.target);
-    }
-  });
-}
-// Función para animar los elementos cuando entran en el viewport
-document.addEventListener("DOMContentLoaded", function () {
-  AOS.init();
-});
-
-// Configurar IntersectionObserver
-const observer = new IntersectionObserver(animateOnScroll, {
-  root: null,
-  rootMargin: '0px',
-  threshold: 0.2 // Cambiar según sea necesario
-});
-
-// Seleccionar los elementos que deseas animar
-const elementsToAnimate = document.querySelectorAll('.animate-on-scroll');
-
-// Observar cada elemento para animarlos cuando entran en el viewport
-elementsToAnimate.forEach(element => {
-  observer.observe(element);
-});
-
 document.getElementById('themeToggle').addEventListener('change', function () {
   if (!this.checked) {
     document.body.classList.add('theme-dark');
