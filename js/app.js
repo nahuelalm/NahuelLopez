f2cef6990e77472a2f7c4fd5fa8feca5a560cf55
Nahuel López <nahuioh.1891@gmail.com>
2024-04-02 17:58:46 -0300

    cambios

--- js/app.js
+++ js/app.js
@@ -1,34 +1,38 @@
-document.getElementById('themeToggle').addEventListener('change', function () {
-  if (!this.checked) {
-    document.body.classList.add('theme-dark');
-    document.body.classList.remove('theme-light');
-  } else {
-    document.body.classList.add('theme-light');
-    document.body.classList.remove('theme-dark');
-  }
-});
-
 document.addEventListener('DOMContentLoaded', function () {
   const themeToggle = document.getElementById('themeToggle');
   const sunIcon = document.querySelector('#sun-icon');
   const moonIcon = document.querySelector('#moon-icon');
 
-  // Invertir los estados iniciales de los iconos
-  sunIcon.classList.add('hidden');
-  moonIcon.classList.remove('hidden');
-
-  // Agregar un event listener para detectar cambios en el interruptor
-  themeToggle.addEventListener('change', function () {
-    if (!themeToggle.checked) {
-      // Modo oscuro activado
+  // Función para actualizar el tema y los iconos
+  function updateTheme(isDarkMode) {
+    if (isDarkMode) {
+      document.body.classList.add('theme-dark');
+      document.body.classList.remove('theme-light');
       sunIcon.classList.add('hidden');
       moonIcon.classList.remove('hidden');
     } else {
-      // Modo claro activado
+      document.body.classList.add('theme-light');
+      document.body.classList.remove('theme-dark');
       sunIcon.classList.remove('hidden');
       moonIcon.classList.add('hidden');
     }
+  }
+
+  // Verificar si hay un estado almacenado previamente
+  const isDarkMode = localStorage.getItem('isDarkMode') === 'true';
+  themeToggle.checked = isDarkMode;
+  updateTheme(isDarkMode);
+
+  // Agregar un event listener para detectar cambios en el interruptor
+  themeToggle.addEventListener('click', function () {
+    const isDarkMode = themeToggle.checked;
+    localStorage.setItem('isDarkMode', isDarkMode);
+    updateTheme(isDarkMode);
+  });
+
+  // Restaurar el tema al cambiar de página
+  window.addEventListener('load', function () {
+    const isDarkMode = localStorage.getItem('isDarkMode') === 'true';
+    updateTheme(isDarkMode);
   });
 });
-
-
